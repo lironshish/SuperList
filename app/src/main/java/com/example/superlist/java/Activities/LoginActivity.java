76af99620893d318a95665dd1bd2f827acd4a777
lironshish <lironshish1@gmail.con>
2022-05-30 22:44:04 +0300

    connect to firebase

--- app/src/main/java/com/example/superlist/java/Activities/LoginActivity.java
+++ app/src/main/java/com/example/superlist/java/Activities/LoginActivity.java
@@ -1,47 +1,111 @@
 package com.example.superlist.java.Activities;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.superlist.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
-    private ImageView sign_up_IMG_background;
-    private ImageView sign_up_IMG_list;
-    private ImageView splash_IMG_logo;
-    private TextView sign_up_TXT_title;
-    private EditText sign_up_LBL_phone_number;
-    private MaterialButton sign_up_BTN_Login;
+    private ImageView login_IMG_background;
+    private ImageView login_IMG_logo;
+    private MaterialButton login_BTN_login;
+
+    //Authentication
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Authentication
+        mAuth = FirebaseAuth.getInstance(); //Init
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+        //    reload();
+        }
+
+
         findViews();
         initButtons();
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+
+
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result){
+
+    }
+
+    private void signUp(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.ic_logo)
+                .build();
+
+        signInLauncher.launch(signInIntent);
+
+
     }
 
     public void findViews(){
-        sign_up_IMG_background = findViewById(R.id.sign_up_IMG_background);
-        sign_up_IMG_list = findViewById(R.id.sign_up_IMG_list);
-        splash_IMG_logo = findViewById(R.id.splash_IMG_logo);
-        sign_up_TXT_title = findViewById(R.id.sign_up_TXT_title);
-   //     sign_up_LBL_phone_number = findViewById(R.id.sign_up_LBL_phone_number);
-        sign_up_BTN_Login = findViewById(R.id.sign_up_BTN_Login);
+        login_IMG_background = findViewById(R.id.login_IMG_background);
+        login_IMG_logo = findViewById(R.id.login_IMG_logo);
+        login_BTN_login = findViewById(R.id.login_BTN_login);
     }
+
+
     public void initButtons() {
-        sign_up_BTN_Login.setOnClickListener(new View.OnClickListener() {
+        login_BTN_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                replaceActivity("buttons");
+                signUp();
             }
-        });
+            });
     }
 }

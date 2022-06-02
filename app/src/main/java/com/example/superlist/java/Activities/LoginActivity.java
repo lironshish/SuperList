985beaaa5c99f78e626000745eab0da779ce025a
lironshish <lironshish1@gmail.con>
2022-06-02 12:07:02 +0300

    Sign Up page design

--- app/src/main/java/com/example/superlist/java/Activities/LoginActivity.java
+++ app/src/main/java/com/example/superlist/java/Activities/LoginActivity.java
@@ -2,29 +2,19 @@ package com.example.superlist.java.Activities;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.superlist.R;
+import com.example.superlist.java.Finals.Keys;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
@@ -32,6 +22,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.security.Key;
 import java.util.Arrays;
 import java.util.List;
 
@@ -42,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
 
     //Authentication
     private FirebaseAuth mAuth;
+
+    //DB
+    FirebaseFirestore db;
 
 
     @Override
@@ -54,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
         //    reload();
+            //TODO: Add function that check if is new user
         }
 
 
@@ -75,10 +70,46 @@ public class LoginActivity extends AppCompatActivity {
     );
 
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result){
-
+        if (result.getResultCode() == RESULT_OK) {
+//            panel_BTN_login.setVisibility(View.INVISIBLE);
+//            panel_PRG_bar.setVisibility(View.VISIBLE);
+            checkIfUserExistInDB();
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 
-    private void signUp(){
+
+    private void checkIfUserExistInDB() {
+
+        startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+
+//        // Successfully signed in
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        DocumentReference docRef = db.collection(Keys.KEY_USERS).document(user.getUid());
+//        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//            @Override
+//            public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                if (documentSnapshot.exists()) {
+//                    Log.d("pttt", "DocumentSnapshot data: " + documentSnapshot.getData());
+////                    MyUser loadedUser = documentSnapshot.toObject(MyUser.class);
+////                    MyDataManager.getInstance().setCurrentUser(loadedUser);
+//                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+//                } else {
+//                    Log.d("pttt", "No such document");
+//                    Log.d("pttt", user.getUid());
+//                    startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+//                }
+//                finish();
+//            }
+//
+//        });
+    }
+
+    private void login(){
         List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
 
 
@@ -90,7 +121,6 @@ public class LoginActivity extends AppCompatActivity {
         //signInIntent.addFlags(PendingIntent.FLAG_IMMUTABLE);
 
         signInLauncher.launch(signInIntent);
-
 
     }
 
@@ -105,7 +135,9 @@ public class LoginActivity extends AppCompatActivity {
         login_BTN_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signUp();
+                login();
+        //        startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+
             }
             });
     }
---
+++ app/src/main/java/com/example/superlist/java/Finals/Keys.java
@@ -0,0 +1,8 @@
+package com.example.superlist.java.Finals;
+
+public class Keys {
+
+    //KEYS
+    public static final String KEY_USERS = "users";
+
+}
